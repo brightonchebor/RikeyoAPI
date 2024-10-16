4e4074fd3676f1c7fb53e0b440b9460fbf97a8d9
Chebor <cheborbrighton805@gmail.com>
2024-10-16 13:01:41 +0300

    cn

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,15 +2,12 @@ from django.contrib import admin
 from .models import *
 
 
-
+admin.site.register(OneTimePassword)
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'date_joined', 'last_login']
 
-admin.site.register(OneTimePassword)
-@admin.register(OneTimePassword)
-class UserOneTimePassword(admin.ModelAdmin):
-    list_display = ['all']
+
 
     
