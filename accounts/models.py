62ea2592cdb5a9c69215b2de32f94623ddb93a1b
brightonchebor <cheborbrighton805@gmail.com>
2024-10-22 15:48:40 +0300

    cn

--- accounts/models.py
+++ accounts/models.py
@@ -44,3 +44,14 @@ class OneTimePassword(models.Model):
 
     def __srt__(self):
         return f'{self.user.first_name}-passcode'    
+
+class Attendance(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f'{self.user.first_name}, {self.user.last_name}'
+    
