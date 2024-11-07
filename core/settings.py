efafc40ec2d12d3e1a4285c92483da27d69100fb
brightonchebor <cheborbrighton805@gmail.com>
2024-11-07 16:52:45 +0300

    cn

--- core/settings.py
+++ core/settings.py
@@ -188,7 +188,7 @@ EMAIL_USE_SSL = False
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "AUTH_HEADER_TYPES": ("Bearer",),
     }
