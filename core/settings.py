e44b008eeed455e5f41f1257525469b57170b6b4
brightonchebor <cheborbrighton805@gmail.com>
2025-01-30 17:38:42 +0300

    backend email config

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,6 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
